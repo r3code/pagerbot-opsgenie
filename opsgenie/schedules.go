73082e01e8d42afaa589b444d71a48738b12afaf
Dmitriy S. Sinyavskiy <contact@r3code.ru>
2022-04-23 23:23:28 +0300

    Create schedules.go

---
+++ opsgenie/schedules.go
@@ -0,0 +1,127 @@
+package opsgenie
+
+import (
+	"time"
+
+	"github.com/opsgenie/opsgenie-go-sdk-v2/schedule"
+	log "github.com/sirupsen/logrus"
+)
+
+type Schedules []Schedule
+
+type Schedule struct {
+	Id            string
+	Name          string
+	Timezone      string `json:"time_zone"`
+	CurrentPeriod *CallPeriod
+	NextPeriod    *CallPeriod
+}
+
+type CallPeriod struct {
+	Start time.Time
+	User  string
+}
+
+// Fetch the main schedule list then the details about specific schedules
+func (a *Api) Schedules() (Schedules, error) {
+	var schdList Schedules
+	var opts opsgenie.ListSchedulesOptions
+
+	for {
+		res, err := a.client.ListSchedules(opts)
+		if err != nil {
+			return schdList, err
+		}
+
+		err = updateScheduleListFromResponse(a, res, &schdList)
+		if err != nil {
+			return schdList, err
+		}
+
+		if !res.More {
+			break
+		}
+
+		opts.Offset = res.Offset + res.Limit
+	}
+
+	return schdList, nil
+}
+
+func updateScheduleListFromResponse(a *Api, res *opsgenie.ListSchedulesResponse, schdList *Schedules) error {
+	var today string = time.Now().UTC().Format("2006-01-02")
+	var nextWeek string = time.Now().UTC().Add(time.Hour * 24 * 7).Format("2006-01-02")
+
+	for _, bareSchedule := range res.Schedules {
+		schd := &Schedule{}
+
+		schd.Id = bareSchedule.ID
+		schd.Name = bareSchedule.Name
+		schd.Timezone = bareSchedule.TimeZone
+
+		res, err := a.client.GetSchedule(bareSchedule.ID, opsgenie.GetScheduleOptions{
+			TimeZone: a.timezone,
+			Since:    today,
+			Until:    nextWeek,
+		})
+		if err != nil {
+			return err
+		}
+
+		var activeEntries int
+		for _, se := range res.FinalSchedule.RenderedScheduleEntries {
+			start, err := time.Parse(time.RFC3339Nano, se.Start)
+			if err != nil {
+				return err
+			}
+
+			end, err := time.Parse(time.RFC3339Nano, se.End)
+			if err != nil {
+				return err
+			}
+
+			if start.Before(time.Now().UTC()) && end.After(time.Now().UTC()) {
+				if activeEntries == 0 {
+					p := CallPeriod{}
+					p.Start = start
+					p.User = se.User.ID
+					schd.CurrentPeriod = &p
+				}
+				activeEntries += 1
+			}
+
+			if start.After(time.Now().UTC()) && (schd.NextPeriod == nil || start.Before(schd.NextPeriod.Start)) {
+				p := CallPeriod{}
+				p.Start = start
+				p.User = se.User.ID
+				schd.NextPeriod = &p
+			}
+		}
+
+		*schdList = append(*schdList, *schd)
+
+		lf := log.Fields{
+			"id": schd.Id,
+		}
+
+		if schd.CurrentPeriod == nil {
+			log.WithFields(lf).Warning("No active current period for schedule")
+		} else {
+			lf["currentCall"] = schd.CurrentPeriod.User
+		}
+
+		if schd.NextPeriod == nil {
+			log.WithFields(lf).Warning("No active next period for schedule")
+		} else {
+			lf["nextCall"] = schd.NextPeriod.User
+			lf["changeover"] = schd.NextPeriod.Start
+		}
+
+		if activeEntries > 1 {
+			log.WithFields(lf).Warning("Multiple active schedules")
+		}
+		log.WithFields(lf).Debug("Got schedule entries")
+	}
+
+	return nil
+}
